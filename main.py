695338332eaa7664b93022e0f85a7d247bbed794
Nadiia Duiunova <nadiia.duiunova@gmail.com>
2023-05-10 23:58:18 +0200

    Add countries mapping

--- main.py
+++ main.py
@@ -1,64 +1,93 @@
 from fastapi import FastAPI
 import pandas as pd
-import numpy as np
-from common_functions import get_clean_data, cluster_categorical
-from sklearn.preprocessing import OrdinalEncoder
 from sklearn.model_selection import train_test_split
+from common_functions import cluster_categorical, cluster_education
 from joblib import load
 
 app = FastAPI()
 
-data = get_clean_data()
+# read precleaned data
+data = pd.read_csv("./clean_data.csv")
 TARGET = 'Income'
 
+# remove examples that do not earn
 data = data[~data['Workclass'].isin(['Never-worked', 'Without-pay'])]
 
-def cluster_education(df):
-    df.loc[
-        lambda x: x["Education-Num"].between(0, 8, "both"), "Education"
-    ] = "Under-grad"
+# cluster Education values to 4 categories:
+data = cluster_education(data)
 
-    df.loc[
-        lambda x: x["Education-Num"] == 9, "Education"
-    ] = "HS-grad"
-
-    df.loc[
-        lambda x: x["Education-Num"] == 10, "Education"
-    ] = "Some-college"
-
-    df.loc[
-        lambda x: x["Education-Num"].between(11, 16, 'both'), "Education"
-    ] = "Above-grad"
-
-    scale_mapper = {'Under-grad':0, 'Some-college':1, 'HS-grad':2, 'Above-grad':3}
-    df["Education"] = df["Education"].replace(scale_mapper)
-
-cluster_education(data)
+# cluster Workclass, Marital status, Relationship and Country values
 data = cluster_categorical(data)
-data = data.drop(columns=['Education-Num'])
 
-X = data.drop(columns=[TARGET])
+# separate dataset to features and target
+X = data.drop(columns=[TARGET, 'Education-Num'])
 y = data[TARGET]
 
 X_train, X_cv, y_train, y_cv = train_test_split(X, y, test_size=0.2)
 
+# create list of developed countries
+countries = pd.read_csv("./country.csv")
+developed_countries = []
+for i in range(countries.shape[0]):
+    if countries['developed'][i]:
+        developed_countries.append(countries['value'][i])
+
+# load pretrained model
 model = load('rand_forest_full.joblib')
 
+# define target function
 def predict_income(age: int, workclass:str, education: str, marital_status: str, occupation: str, relationship: str, 
                    ethnic_group: str, sex: str, country: str, capital_gain: int = 0, capital_loss: int = 0, hours_per_week: int = 40):
+    """_summary_
+
+    Args:
+        age: int
+            user's age
+        workclass: str
+            user's workclass
+        education: str
+            user's education
+        marital_status: str 
+            user's marital status
+        occupation: str 
+            user's work occupation
+        relationship: str 
+            whether or not user belongs to the family
+        ethnic_group: str 
+            user's ethnic group (as initialy defined in dataset)
+        sex: str
+            user's sex
+        country: str 
+            user's country of living and working
+        capital_gain: int, optional 
+            positive number, set if sum of all operations with capital led to profit. Defaults to 0.
+        capital_loss: int, optional 
+            positive number, set if sum of all operations with capital led to los. Defaults to 0.
+        hours_per_week: int, optional 
+            user's working hours per week. Defaults to 40.
+
+    Returns:
+        dict 
+            Dictionary with 2 values: result, containing the predicted class of income; and probability of this prediction to be true, always > 50% 
+    """
+    
     model.fit(X_train, y_train)
 
-    # transform data from user input to the same clusters as train data
-    # education
-    # marital status
-    # relationship
-    # country
+    # transform data from user input to the same clusters as in train data
+    if country in developed_countries:
+        country = 'Developed'
+    else:
+        country = 'Developing'
     
-
-    X_predictable = pd.DataFrame([[age, workclass, education, marital_status, occupation, relationship, ethnic_group, sex, capital_gain, capital_loss, hours_per_week, country]], columns=X_train.columns)
+    # create dataframe with user's data
+    X_predictable = pd.DataFrame([[age, workclass, education, marital_status, occupation, relationship, ethnic_group, 
+                                   sex, capital_gain, capital_loss, hours_per_week, country]], columns=X_train.columns)
+    
+    # predict income class
     income_prediction = model.predict(X_predictable)[0]
-    prediction_prob = model.predict_proba(X_predictable)
 
+    # get probability of that prediction
+    prediction_prob = model.predict_proba(X_predictable)
     if income_prediction == '>50K':
         prediction_prob = prediction_prob[0][1]
     else:
@@ -66,8 +95,6 @@ def predict_income(age: int, workclass:str, education: str, marital_status: str,
 
     return {'result': income_prediction,
             'prediction_prob': float(prediction_prob)*100}
-
-
 
 
 @app.get("/predict_induvidual_income/")
