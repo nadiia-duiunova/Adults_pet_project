d2801ba53341acc1d748d6f5b66f4d9044af46d5
Nadiia Duiunova <nadiia.duiunova@gmail.com>
2023-05-13 22:18:42 +0200

    Add shapley values to rusulting json

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 import pandas as pd
-from sklearn.model_selection import train_test_split
+import numpy as np
 from common_functions import cluster_categorical, cluster_education
 from joblib import load
+import shap
 
 app = FastAPI()
 
@@ -10,7 +11,7 @@ app = FastAPI()
 data = pd.read_csv("./clean_data.csv")
 TARGET = 'Income'
 
-# remove examples that do not earn
+# remove examples that do not earn money
 data = data[~data['Workclass'].isin(['Never-worked', 'Without-pay'])]
 
 # cluster Education values to 4 categories:
@@ -22,8 +23,6 @@ data = cluster_categorical(data)
 # separate dataset to features and target
 X = data.drop(columns=[TARGET, 'Education-Num'])
 y = data[TARGET]
-
-X_train, X_cv, y_train, y_cv = train_test_split(X, y, test_size=0.2)
 
 # create list of developed countries
 countries = pd.read_csv("./country.csv")
@@ -37,8 +36,8 @@ model = load('rand_forest_full.joblib')
 
 # define target function
 def predict_income(age: int, workclass:str, education: str, marital_status: str, occupation: str, relationship: str, 
-                   ethnic_group: str, sex: str, country: str, capital_gain: int = 0, capital_loss: int = 0, hours_per_week: int = 40):
-    """_summary_
+                   ethnic_group: str, sex: str, country: str, capital_gain: int, capital_loss: int, hours_per_week: int):
+    """ Predict user's income based on given parametes
 
     Args:
         age: int
@@ -60,18 +59,19 @@ def predict_income(age: int, workclass:str, education: str, marital_status: str,
         country: str 
             user's country of living and working
         capital_gain: int, optional 
-            positive number, set if sum of all operations with capital led to profit. Defaults to 0.
+            positive number, set if sum of all operations with capital led to profit
         capital_loss: int, optional 
-            positive number, set if sum of all operations with capital led to los. Defaults to 0.
+            positive number, set if sum of all operations with capital led to los
         hours_per_week: int, optional 
-            user's working hours per week. Defaults to 40.
+            user's working hours per week
 
     Returns:
         dict 
             Dictionary with 2 values: result, containing the predicted class of income; and probability of this prediction to be true, always > 50% 
     """
     
-    model.fit(X_train, y_train)
+    # model.fit(X_train, y_train)
+    model.fit(X, y)
 
     # transform data from user input to the same clusters as in train data
     if country in developed_countries:
@@ -81,23 +81,53 @@ def predict_income(age: int, workclass:str, education: str, marital_status: str,
     
     # create dataframe with user's data
     X_predictable = pd.DataFrame([[age, workclass, education, marital_status, occupation, relationship, ethnic_group, 
-                                   sex, capital_gain, capital_loss, hours_per_week, country]], columns=X_train.columns)
+                                   sex, capital_gain, capital_loss, hours_per_week, country]], columns=X.columns)
     
     # predict income class
     income_prediction = model.predict(X_predictable)[0]
-
-    # get probability of that prediction
     prediction_prob = model.predict_proba(X_predictable)
-    if income_prediction == '>50K':
-        prediction_prob = prediction_prob[0][1]
-    else:
+
+    #explain result with shap values
+    explainer = shap.TreeExplainer(model['randomforestclassifier'])
+    data_point = model['columntransformer'].transform(X_predictable)
+    x_columns_names = model['columntransformer'].get_feature_names_out()
+    data_point = pd.DataFrame(data_point, columns = x_columns_names)
+
+    shap_values = explainer.shap_values(data_point)
+
+    if income_prediction == '<=50K':
         prediction_prob = prediction_prob[0][0]
+        shap_values = shap_values[0]
+        expected_value = explainer.expected_value[0]
+    else:
+        prediction_prob = prediction_prob[0][1]
+        shap_values = shap_values[1]
+        expected_value = explainer.expected_value[1]
+
+    # inverse transformation to original columns
+    n_categories = [1, 1, 1, 1, 5, 1, 13, 1, 4, 1, 1, 1]
+    new_shap_values = []
+    for values in shap_values:
+        #split shap values into a list for each feature
+        values_split = np.split(values , np.cumsum(n_categories))
+        
+        #sum values within each list
+        values_sum = [sum(l) for l in values_split]
+        
+        new_shap_values.append(values_sum)
+
+    new_shap_values = new_shap_values[0][:-1]
+
 
     return {'result': income_prediction,
-            'prediction_prob': float(prediction_prob)*100}
+            'prediction_prob': float(prediction_prob)*100,
+            'shap_values': new_shap_values,
+            'expected_value': expected_value
+            }
+
 
 
 @app.get("/predict_induvidual_income/")
 def predict_income_result(age: int, workclass:str, education: str, marital_status: str, occupation: str, relationship: str, 
-                   ethnic_group: str, sex: str, country: str, capital_gain: int = 0, capital_loss: int = 0, hours_per_week: int = 40):
+                   ethnic_group: str, sex: str, country: str, capital_gain: int, capital_loss: int, hours_per_week: int):
     return predict_income(age, workclass, education, marital_status, occupation, relationship, ethnic_group, sex, country, capital_gain, capital_loss, hours_per_week)
